43495bebd5609f5937f55431cc882eecf6008aff
Kristoffer <kristoffer.niklasson@sogeti.se>
2024-11-21 15:38:00 +0100

    removed magic api

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -17,13 +17,4 @@ export class DataService {
     return this.http.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
   }
 
-  //Magic cards
-  getMagicTheGathering(limit: number, offset: number) {
-    return this.http.get(`https://api.magicthegathering.io/v1/cards?pageSize=${limit}&page=${offset}`);
-  }
-
-  getMoreMagicCards(name: string) {
-    return this.http.get(`https://api.magicthegathering.io/v1/cards?name=${name}`);
-  }
-
 }
