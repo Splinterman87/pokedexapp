d639e100d8274ab929d9166585b3c4e5bb57ce84
Kristoffer <kristoffer.niklasson@sogeti.se>
2024-11-21 16:01:53 +0100

    added search field

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatFormFieldModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
